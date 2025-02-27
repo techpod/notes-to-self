52b52ea48fa38d30a8b6fef4f32c4eedd3f40da3
Cy Caruso <ccaruso@idealss.com>
2025-02-27 09:17:22 -0800

    Add authentication and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
+/*added*/
+import { useAuthenticator } from '@aws-amplify/ui-react';
+/*end added*/
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
+  /*added*/
+  const { signOut } = useAuthenticator();
+  /*end added*/
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -17,13 +23,24 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  /*added*/  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+  /*end added*/
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            /*added*/
+            onClick={() => deleteTodo(todo.id)}
+            /*end added*/
+            key={todo.id}>{todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -33,6 +50,9 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      {/*added*/}
+      <button onClick={signOut}>Sign out</button>
+      {/*end added*/}
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+/*added*/
+import { Authenticator } from '@aws-amplify/ui-react';
+/*end added*/
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+/*added*/
+import '@aws-amplify/ui-react/styles.css';
+/*end added*/
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  /*
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  */
+  /*added*/
+  <React.StrictMode>
+  <Authenticator>
+    <App />
+  </Authenticator>
+  </React.StrictMode>
+  /*end added*/
 );
