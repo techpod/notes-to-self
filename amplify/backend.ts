9a4e920e6cf3c4bf540789435e932d6b85b9ff55
Cy Caruso <ccaruso@idealss.com>
2025-10-19 13:17:29 -0700

    modify process items

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-//defineBackend({
-const be = defineBackend({
+defineBackend({
   auth,
   data,
 });
-be.data.resources.cfnResources.cfnApiKey?.overrideLogicalId(
-      `updateRsrc${new Date().getTime()}`
-);
